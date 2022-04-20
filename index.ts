6eaf4a9eac21d9e26acad5c1d1d11ff53e271838
Jonathan Pulsifer <jonathan@pulsifer.ca>
2022-04-20 16:29:13 -0300

    yeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- index.ts
+++ index.ts
@@ -2,27 +2,26 @@ import fetch from 'cross-fetch';
 import { mkdirSync, writeFileSync } from 'fs';
 import * as path from 'path';
 
-const now = new Date();
-const year = now.getFullYear().toString();
-const day = process.argv[2] || now.getDate().toString();
-const url = `https://adventofcode.com/${year}/day/${day}/input`;
-const cookie = process.env.AOC_SESSION_COOKIE || '';
-if (!cookie) throw new Error('AOC session cookie not set');
+async function fetchInput(url: string, cookie: string): Promise<string> {
+  const headers = {
+    cookie: `session=${cookie}`
+  }
 
-const headers = {
-  cookie: `session=${cookie}`
-}
-
-async function fetchInput(url: string, cookie: string ): Promise<string> {
   const response = await fetch(
     url,
     { headers }
   );
   if (response.ok) return await response.text();
-  throw new Error(`Could not download input for ${year}-${day}`);
+  throw new Error(`Could not download input from ${url}`);
 };
 
 function saveInput(): void {
+  const cookie = process.env.AOC_SESSION_COOKIE || '';
+  if (!cookie) throw new Error('AOC session cookie not set');
+
+  const now = new Date();
+  const year = process.argv[2] || now.getFullYear().toString();
+  const day = process.argv[3] || now.getDate().toString();
   const outputDir = path.resolve('.', year);
   try {
     mkdirSync(outputDir);
@@ -31,6 +30,8 @@ function saveInput(): void {
       throw new Error(`${error.code}: could not create directory`);
     };
   };
+
+  const url = `https://adventofcode.com/${year}/day/${day}/input`;
   fetchInput(url, cookie).then(input => {
     writeFileSync(path.resolve(outputDir, `day${day.padStart(2, '0')}.txt`), input);
     console.log(`Wrote ${input.length} bytes to ${outputDir}`);
